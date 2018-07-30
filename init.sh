efc46d5b037b3c5b444e9419f1f0be86d6dc2425
Dave Welch <david@davidwelch.co>
2018-07-30 09:00:24 -0600

    support for remotes

--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ docker-compose up -d
 
 sleep 1.5
 
-./flyway.sh $1 src/config/usa.properties 1.1
-./flyway.sh $1 src/config/gbr.properties 1.1
-./flyway.sh $1 src/config/aus.properties 1.1
+./flyway.sh $1 src/config/${2:-local}/usa.properties 1.1
+./flyway.sh $1 src/config/${2:-local}/gbr.properties 1.1
+./flyway.sh $1 src/config/${2:-local}/aus.properties 1.1
 
 ./migrate.sh $1
--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 latest=9
-./flyway.sh migrate src/config/usa.properties $latest
-./flyway.sh migrate src/config/gbr.properties $latest
-./flyway.sh migrate src/config/aus.properties $latest
+./flyway.sh ${1:-migrate} src/config/${2:-local}/usa.properties $latest
+./flyway.sh ${1:-migrate} src/config/${2:-local}/gbr.properties $latest
+./flyway.sh ${1:-migrate} src/config/${2:-local}/aus.properties $latest
